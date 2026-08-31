fcee5bcd31766419fe0a553d4278d32b39ef506f
vinicius <vinicius.j2009@gmail.com>
2026-08-31 09:35:37 -0300

    feat: base quase completa

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,72 @@
-import { StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Pressable, Image } from 'react-native'
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+export default function index() {
+    return (
+        <View style={style.container}>
+            <View style={style.navBar}>
+                <View><Text style={{fontWeight:"bold",fontSize:36}}>Titulo</Text></View>
+                <View style={style.navOpt}>
+                    <Pressable><Text>nav</Text></Pressable>
+                    <Pressable><Text>nav</Text></Pressable>
+                    <Pressable><Text>nav</Text></Pressable>
+                </View>
+            </View>
+            <View style={style.carrossel}>
+                <Image source={{ uri:"https://ingresso-a.akamaihd.net/prd/img/movie/patrulha-canina-uma-aventura-dino/1a69bead-abaf-4831-ad0a-a3b98915fc4f.webp" }}></Image>
+            </View>
+            <View style={style.catalogo}>
+                <View style={style.categoria}><Text style={{fontWeight:"bold",fontSize:20}}>categoria 1</Text>
+                </View>
+                <View style={style.categoria}><Text style={{fontWeight:"bold",fontSize:20}}>categoria 2</Text>
+                </View>
+                <View style={style.categoria}><Text style={{fontWeight:"bold",fontSize:20}}>categoria 3</Text>
+                </View>
+                <View style={style.categoria}><Text style={{fontWeight:"bold",fontSize:20}}>categoria 4</Text>
+                </View>
+                
+            </View>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "beige",
+    },
+    navBar: {
+        width: "100%",
+        height: "15%",
+        padding: 20,
+        backgroundColor: "green",
+        display:"flex",
+        alignContent: "flex-end",
+        justifyContent: "space-between"
+    },
+    navOpt: {
+        backgroundColor:"red",
+        width: "100%",
+        gap: 5,
+        display:"flex",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    carrossel: {
+        backgroundColor: "blue",
+        height: "30%",
+        margin: 15,
+        justifyContent:"center",
+        alignContent:"center"
+    },
+    catalogo: {
+        backgroundColor: "purple",
+        margin: 15,
+        display:"flex",
+        justifyContent: "space-between",
+        alignContent: "center"
+    },
+    categoria: {
+        marginBottom: 10,
+
+    }
+})
