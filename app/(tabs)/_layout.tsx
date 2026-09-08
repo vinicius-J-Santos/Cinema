5767ec269be4b2a9f89f18d1f2b59883c9934467
Aluno <Aluno@L2T01.acad.etecpoa>
2026-09-08 12:26:43 -0300

    style:arrumando cores e tamanhos

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import {Tabs} from "expo-router"
 export default function RootLayout() {
 	return (
 		<Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="index" options={{title: "Index"}}/>
+            <Tabs.Screen name="index" options={{title: "Index", tabBarStyle:{display: 'none'}}}/>
         </Tabs>
     )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,35 +4,34 @@ export default function index() {
     return (
         <ScrollView style={style.container}>
             <View style={style.navBar}>
-                <View><Text style={{fontWeight:"bold",fontSize:36}}>Titulo</Text></View>
+                <View><Text style={{fontWeight:"bold",fontSize:36,color:"#fff279ff"}}>C!NEMA</Text></View>
                 <View style={style.navOpt}>
-                    <Pressable><Text>nav</Text></Pressable>
-                    <Pressable><Text>nav</Text></Pressable>
-                    <Pressable><Text>nav</Text></Pressable>
+                    <Pressable><Text style={style.text}>Catalogo</Text></Pressable>
+                    <Pressable><Text style={style.text}>Cinemas</Text></Pressable>
+                    <Pressable><Text style={style.text}>Ajuda</Text></Pressable>
                 </View>
             </View>
             <View style={style.carrossel}>
-                <Text style={{fontWeight:"bold",fontSize:20}}>Destaque do mês:</Text>
+                <Text style={{fontWeight:"bold",fontSize:20,color:"#fff279ff"}}>Destaque do mês:</Text>
                 <Image source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNBLHOGwbG4J61AIcIwjO9eKORkaX5_GC31KDgOtKzA&s=10'}} style={style.imgCaroossel} />            
             </View>
             <View style={style.catalogo}>
-                <Text style={{fontWeight:"bold",fontSize:20}}>categoria 1</Text>
+                <Text style={{fontWeight:"bold",fontSize:20,color:"#fff279ff"}}>categoria 1</Text>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false} style={style.categoria}>
                     {/* componentizar tudo e lembrar de fazer Pressable*/}
                 </ScrollView>
-                <Text style={{fontWeight:"bold",fontSize:20}}>categoria 2</Text>
+                <Text style={{fontWeight:"bold",fontSize:20,color:"#fff279ff"}}>categoria 2</Text>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false} style={style.categoria}>
                     {/* componentizar tudo e lembrar de fazer Pressable*/}
                 </ScrollView>
-                <Text style={{fontWeight:"bold",fontSize:20}}>categoria 3</Text>
+                <Text style={{fontWeight:"bold",fontSize:20,color:"#fff279ff"}}>categoria 3</Text>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false} style={style.categoria}>
                     {/* componentizar tudo e lembrar de fazer Pressable*/}
                 </ScrollView>
-                <Text style={{fontWeight:"bold",fontSize:20}}>categoria 4</Text>
+                <Text style={{fontWeight:"bold",fontSize:20,color:"#fff279ff"}}>categoria 4</Text>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false} style={style.categoria}>
                     {/* componentizar tudo e lembrar de fazer Pressable*/}
-                </ScrollView>
-                            
+                </ScrollView>       
             </View>
         </ScrollView>
     )
@@ -41,29 +40,35 @@ export default function index() {
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "beige",
+        backgroundColor: "#1d79f2ff"
+    },
+    text: {
+        color:"#fff279ff"
     },
     navBar: {
         width: "100%",
         height: 150,
         padding: 20,
-        backgroundColor: "green",
+        backgroundColor: "#1a05a1ff",
         display:"flex",
         alignItems: "flex-start",
         justifyContent: "space-between"
     },
     navOpt: {
-        backgroundColor:"red",
+        backgroundColor: "#0f0274ff",
         width: "100%",
-        gap: 5,
+        padding: 5,
+        borderRadius: 10,
         display:"flex",
         flexDirection: "row",
         justifyContent: "space-between"
     },
     carrossel: {
-        backgroundColor: "blue",
+        backgroundColor: "#1a05a1ff",
         height: "65%",
-        margin: 15,
+        padding:15,
+        margin: 10,
+        borderRadius: 20,
         justifyContent:"space-between",
         alignItems:"flex-start"
     },
@@ -73,8 +78,10 @@ const style = StyleSheet.create({
         borderRadius: 10
     },
     catalogo: {
-        backgroundColor: "purple",
-        margin: 15,
+        backgroundColor: "#1a05a1ff",
+        margin: 10,
+        padding: 15,
+        borderRadius: 20,
         display:"flex",
         justifyContent: "space-between",
         alignContent: "center"
@@ -84,8 +91,6 @@ const style = StyleSheet.create({
         gap:5,
         display:"flex",
         flexDirection:"row",
-        flexWrap:"nowrap",
-
-        
+        flexWrap:"nowrap"
     }
 })
